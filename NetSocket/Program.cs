2a88afe9ba50ae8b38543dcc41f53f0704ffc0b8
Jonas <admin@j-kit.de>
2018-09-24 21:42:57 +0200

    Projektdateien hinzufügen.

---
+++ NetSocket/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
