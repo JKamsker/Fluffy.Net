857448e9b84d7852fd4e566d468fb020c6793676
Kamsker, Jonas <Jonas.Kamsker@beko.at>
2018-11-09 14:03:57 +0100

    Fixed bug which caused bufferFactories not being cached

---
+++ Fluffy.IO/Recycling/Capacity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Fluffy.IO.Recycling
+{
+    public enum Capacity
+    {
+        /// <summary>
+        /// 1 KB
+        /// </summary>
+        ExtraSmall,
+
+        /// <summary>
+        /// 4KB
+        /// </summary>
+        Small,
+
+        /// <summary>
+        /// 4 KB
+        /// </summary>
+        Medium,
+
+        /// <summary>
+        /// 8 KB
+        /// </summary>
+        Big
+    }
+
+    public static class CapacityExtension
+    {
+        public static int ToInt(this Capacity capacity)
+        {
+            int size = 32;
+
+            switch (capacity)
+            {
+                case Capacity.ExtraSmall:
+                    size = 1 * 1024;
+                    break;
+
+                case Capacity.Small:
+                    size = 2 * 1024;
+                    break;
+
+                case Capacity.Medium:
+                    size = 4 * 1024;
+
+                    break;
+
+                case Capacity.Big:
+                    size = 8 * 1024;
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(capacity), capacity, null);
+            }
+
+            return size;
+        }
+    }
+}
--- Fluffy.IO/Recycling/ObjectRecyclingFactoryFactory.cs
+++ Fluffy.IO/Recycling/ObjectRecyclingFactoryFactory.cs
@@ -1,63 +1,33 @@
 ﻿using Fluffy.IO.Buffer;
 
 using System;
+using System.Linq;
 
 namespace Fluffy.IO.Recycling
 {
-    public enum Capacity
-    {
-        /// <summary>
-        /// 1 KB
-        /// </summary>
-        ExtraSmall,
-
-        /// <summary>
-        /// 4KB
-        /// </summary>
-        Small,
-
-        /// <summary>
-        /// 4 KB
-        /// </summary>
-        Medium,
-
-        /// <summary>
-        /// 8 KB
-        /// </summary>
-        Big
-    }
-
     /// <summary>
     /// Provides an easy way to access a pool of shared buffer objects
     /// </summary>
     public static class BufferRecyclingMetaFactory
     {
+        private static BufferRecyclingFactory<LinkableBufferObject<byte>>[] _factories;
+
+        static BufferRecyclingMetaFactory()
+        {
+            var max = (int)Enum.GetValues(typeof(Capacity)).Cast<Capacity>().Max();
+            _factories = new BufferRecyclingFactory<LinkableBufferObject<byte>>[max];
+        }
+
         public static IObjectRecyclingFactory<LinkableBufferObject<byte>> Get(Capacity capacity)
         {
-            int size = 32;
-            switch (capacity)
+            int iCapacity = (int)capacity;
+
+            if (_factories[iCapacity] == null)
             {
-                case Capacity.ExtraSmall:
-                    size = 1 * 1024;
-                    break;
-
-                case Capacity.Small:
-                    size = 2 * 1024;
-                    break;
-
-                case Capacity.Medium:
-                    size = 4 * 1024;
-
-                    break;
-
-                case Capacity.Big:
-                    size = 8 * 1024;
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(capacity), capacity, null);
+                _factories[iCapacity] = new BufferRecyclingFactory<LinkableBufferObject<byte>>(capacity.ToInt());
             }
-            return new BufferRecyclingFactory<LinkableBufferObject<byte>>(size);
+
+            return _factories[iCapacity];
         }
     }
 }
