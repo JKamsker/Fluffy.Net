ff3c59769a0e4d7a665485b3beedf9ce66a3f502
Jonas <darkie@j-kit.de>
2018-11-18 19:20:46 +0100

    Things

---
+++ FluffySock.Net/Packets/IPacketResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Fluffy.Net.Packets
+{
+    public interface IPacketResult<TResult>
+    {
+        TResult Result { get; }
+        Task<TResult> ResultTask { get; }
+    }
+}
