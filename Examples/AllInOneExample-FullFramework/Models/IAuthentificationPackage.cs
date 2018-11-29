ab2995cff03082b25a5c254c0f623948cd9acc7c
Jonas <darkie@j-kit.de>
2018-11-29 12:49:01 +0100

    Temporarily Removed test

---
+++ Examples/AllInOneExample-FullFramework/Models/IAuthentificationPackage.cs
@@ -0,0 +1,12 @@
+﻿namespace AllInOneExample_FullFramework.Models
+{
+    public interface IAuthenticationPacket
+    {
+    }
+
+    public class NamePasswordAuthentication : IAuthenticationPacket
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
