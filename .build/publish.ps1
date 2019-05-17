7d042ce7c102d15a7fd42f01e509c0e7f00b6c4d
Jonas kamsker <jonaskamsker@hotmail.com>
2019-05-18 00:48:45 +0200

    testcommit

--- .build/publish.ps1
+++ .build/publish.ps1
@@ -1,3 +1,22 @@
+param (
+    [switch]$xx,
+    [switch]$xy,
+    [String]$creds
+)
+
+if ($xx) {
+    Write-Host "XX set"
+}else{
+    Write-Host "XX not set"
+}
+if ($xy) {
+    Write-Host "xy set"
+}else{
+    Write-Host "xy not set"
+}
+
+Write-Host "Creds: $creds"
+
 $items = Get-Item ..\*\*\ReleaseBuild\*.nupkg
 
 if (Get-Command "nuget.exe" -ErrorAction SilentlyContinue) {
