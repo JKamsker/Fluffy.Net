c9ef413695a2c9fb1844ad7d9905cd8e708a8982
Jonas kamsker <jonaskamsker@hotmail.com>
2019-05-18 01:27:51 +0200

    tst

--- .build/publish.ps1
+++ .build/publish.ps1
@@ -9,7 +9,7 @@ if ((Get-Command "nuget.exe" -ErrorAction SilentlyContinue) -eq $null) {
 $items = Get-Item ..\*\*\ReleaseBuild\*.nupkg
 foreach ($item in $items) {
    Write-Host "Publishing $($item.Name)"
-   nuget push $($item.FullName) -Source "https://api.nuget.org/v3/index.json" -ApiKey $creds -Verbosity detailed
+   nuget push $($item.FullName) -Source "https://api.nuget.org/v3/index.json" -ApiKey "$creds" -Verbosity detailed
 }
 
 
