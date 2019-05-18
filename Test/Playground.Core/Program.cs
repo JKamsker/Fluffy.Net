90a6499b1cd786b9ab8139db859cb981e4387182
Jonas kamsker <jonaskamsker@hotmail.com>
2019-05-18 04:09:18 +0200

    Removed netcoreapp again

--- Test/Playground.Core/Program.cs
+++ Test/Playground.Core/Program.cs
@@ -5,6 +5,7 @@ using System.Security.Permissions;
 namespace Playground.Core
 {
     // [HostProtection(SecurityAction.LinkDemand, ExternalThreading = true, Synchronization = true)]
+    [Serializable]
     class Program
     {
         static void Main(string[] args)
